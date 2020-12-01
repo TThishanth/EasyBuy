259795641422d65273c397f9f9b7cf470abdb1e6
Thishanth98 <thishanth41@gmail.com>
2020-12-01 12:17:52 +0530

    Second Commit

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Authentication'),
+      ),
+    );
+  }
+}
